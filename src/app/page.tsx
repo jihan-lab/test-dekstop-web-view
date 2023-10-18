2376d4c557e36466afc7157e8479f378b62b5ab6
jihan abdul <jihannabdul@gmail.com>
2023-10-18 09:15:23 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Ini tampilan Web desktop view</h1>
+    </main>
+  );
+}
