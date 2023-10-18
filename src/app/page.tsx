a5384519b1a7ad370736fc844caa75f7fb094f3e
jihan-lab <jihannabdul@gmail.com>
2023-10-18 16:09:51 +0700

    chore: add react detect device, for serv 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { isMobile, isAndroid, isIOS } from "react-device-detect";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    if (isAndroid || isAndroid || isMobile) {
+      location.replace("http://172.20.65.245:3000");
+    }
+  }, []);
+
   return (
     <main className={styles.main}>
-      <h1>Ini tampilan Web desktop view server 2</h1>
+      <h1>Ini tampilan Web desktop view server 1</h1>
     </main>
   );
 }
